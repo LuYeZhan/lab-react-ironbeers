11a2e092cc616a998b8bebb68342fe4017094d6a
Lu <lu@hooli.store>
2019-08-08 23:14:33 +0200

    first commit

---
+++ reactive-beersjs/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <img src="" alt=""/>
+      <h2>All beers</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio cupiditate beatae, minus optio voluptatum, fugiat ratione sed sunt sit ducimus cum ipsam eveniet dolor non impedit nihil veniam tenetur?</p>
+      <img src="" alt=""/>
+      <h2>Random pages</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio cupiditate beatae, minus optio voluptatum, fugiat ratione sed sunt sit ducimus cum ipsam eveniet dolor non impedit nihil veniam tenetur?</p>
+      <img src="" alt=""/>
+      <h2>New beer</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio cupiditate beatae, minus optio voluptatum, fugiat ratione sed sunt sit ducimus cum ipsam eveniet dolor non impedit nihil veniam tenetur?</p>
+    </div>
+  )
+}
+
+export default Home;
